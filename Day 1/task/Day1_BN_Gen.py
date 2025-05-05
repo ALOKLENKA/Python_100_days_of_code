b2204857aea24dec713ee8eea4a67dad9f4beacc
ALOKLENKA <55556688+ALOKLENKA@users.noreply.github.com>
2025-05-05 20:31:16 +0530

    Add files via upload

---
+++ Day 1/task/Day1_BN_Gen.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand name genarator !")
+city=input("Which city you were born in:")
+pet=input("Whats our pet name:")
+print("Your Brand Name is:" + city + " " + pet)
---
+++ Day 1/task/solution.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Hello world!")
+print (len(input("What is your Name:")) )
+username=input("What is your Name:")
+length=len(username)
+print("Username is :" + username + "lengh is:"+ str(length))
