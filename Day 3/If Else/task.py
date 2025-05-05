59da495b14db878e7b3c64c5224d03c73cb358a9
ALOKLENKA <55556688+ALOKLENKA@users.noreply.github.com>
2025-05-05 20:44:51 +0530

    Add files via upload

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,19 @@
+# age= int(input("enter your age:"))
+# if age <=2:
+#     print( "you are a baby")
+# elif age >2 and age <=12 :
+#     print("you ae a kid")
+# elif age>12 and age <=19:
+#     print("you area teen ager")
+# elif age>19 and age <50:
+#     print("you are an adult")
+# else:
+#     print("you are old")
+
+num=int(input("Enter the number:"))
+
+
+if(num % 2 == 0):
+    print("its an even number")
+else:
+    print("its an odd number")
